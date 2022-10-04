244a63ea6507d1f51ffa7d56c9f61d3d7e865dc8
chrisemezue <chris.emezue@gmail.com>
2022-10-04 14:51:31 -0400

    updates with comment

--- testgr.py
+++ testgr.py
@@ -7,7 +7,11 @@ import networkx as nx
 from dowhy import CausalModel
 import pickle as pl
 from joblib import Parallel, delayed
+from joblib.externals.loky import set_loky_pickler
+from joblib import wrap_non_picklable_objects
 from typing import Optional
+
+set_loky_pickler('pickle5') #https://joblib.readthedocs.io/en/latest/auto_examples/serialization_and_wrappers.html
 
 node_to_index = {
     'A':0,
@@ -25,7 +29,7 @@ seeds_ = []
 BASELINE_FOLDER = '/home/mila/c/chris.emezue/scratch/baselines/'
 
 
-
+@wrap_non_picklable_objects
 def get_causal_estimate(graph,df):
     model = CausalModel(data=df, treatment=['K'],outcome='M',graph=nx.DiGraph(graph),use_graph_as_is=True)
     # II. Identify causal effect and return target estimands
@@ -39,7 +43,10 @@ def get_causal_estimate(graph,df):
     causal_estimate = causal_estimate_reg.value
     return causal_estimate
 
-def get_estimate_from_posterior(each_posterior,index_to_node,df):
+@wrap_non_picklable_objects
+def get_estimate_from_posterior(each_posterior,index_to_node,BASE_PATH):
+    df = pd.read_csv(os.path.join(BASE_PATH,'data.csv'))
+
     graph_sample = nx.from_numpy_array(each_posterior,create_using=nx.DiGraph)
     if nx.is_directed_acyclic_graph(graph_sample): # Check it is acyclic DAG
         graph_sample_relabeled = nx.relabel_nodes(graph_sample, index_to_node)
@@ -76,9 +83,17 @@ def calculate_squared_diff(a: np.ndarray, b: np.ndarray, axis: Optional[int] = N
     """
     return np.square(np.subtract(a, b))
 
+#0 5 10 15 20 25
+0,1,2,3,4
+5,6,7,8,9
 if __name__=="__main__":
     baseline_to_use = sys.argv[1]
-    SEED_TO_USE = [sys.argv[2]]
+    SEED_TO_USE = []
+
+    seed_number = int(sys.argv[2])
+    if seed_number != 0:
+        SEED_TO_USE = [i for i in range(seed_number-5,seed_number)]
+
     for baseline in [baseline_to_use]:
         for seed in SEED_TO_USE:
             BASE_PATH = os.path.join(os.path.join(BASELINE_FOLDER,baseline),str(seed))
@@ -89,33 +104,38 @@ if __name__=="__main__":
             with open(os.path.join(BASE_PATH,'graph.pkl'),'rb') as fl:
                 graph = pl.load(fl)
 
-            df = pd.read_csv(os.path.join(BASE_PATH,'data.csv'))
-
-            true_estimate = get_causal_estimate(graph,df)
 
             # Get posterior
             count=0
             posterior_file_path = os.path.join(BASE_PATH,'posterior.npy')
             if not os.path.isfile(posterior_file_path):
                 continue
-            posterior = np.load(posterior_file_path)[:16,:,:]
-            #causal_estimates = np.array([get_estimate_from_posterior(posterior[i,:,:]) for i in range(posterior.shape[0])])
-            import pdb;pdb.set_trace()
+            #posterior = np.load(posterior_file_path)
+            posterior = np.load(posterior_file_path)[:10,:,:]
+            #st_time = time.time()
+            #causal_estimates = np.array([get_estimate_from_posterior(posterior[i,:,:],index_to_node,BASE_PATH) for i in range(posterior.shape[0])])
+
             #print(f'Time taken without threading: {time.time()-st_time}')
-            st_time = time.time()
+            #st_time = time.time()
             #len(os.sched_getaffinity(0))
-            results = Parallel(n_jobs=2)(
-                    delayed(get_estimate_from_posterior)(posterior[i,:,:],index_to_node,df)
+            results = Parallel(n_jobs=len(os.sched_getaffinity(0)))(
+                    delayed(get_estimate_from_posterior)(posterior[i,:,:],index_to_node,BASE_PATH)
                     for i in range(posterior.shape[0])
                     )
-            import pdb;pdb.set_trace()
-            print(f'Time taken for one posterior of size {posterior.shape[0]} was {time.time()-st_time}')
+            #print(f'Time taken WITH threading: {time.time()-st_time}')
+            causal_estimates = np.asarray(results)
+            df = pd.read_csv(os.path.join(BASE_PATH,'data.csv'))
 
-            with open(f'/home/mila/c/chris.emezue/scratch/ate_estimates2/{baseline_to_use}_{seed}_ate_estimates.npy', 'wb') as fl:
+
+            true_estimate = get_causal_estimate(graph,df)
+
+            #print(f'Time taken for one posterior of size {posterior.shape[0]} was {time.time()-st_time}')
+
+            with open(f'/home/mila/c/chris.emezue/scratch/ate_estimates3/{baseline_to_use}_{seed}_ate_estimates.npy', 'wb') as fl:
                 np.save(fl,causal_estimates)
             #causal_estimates = np.full(posterior.shape[0], fill_value=1) 
             true_causal_estimates = np.full(causal_estimates.shape, fill_value=true_estimate)
-            with open(f'/home/mila/c/chris.emezue/scratch/ate_estimates2/true_{baseline_to_use}_{seed}_ate_estimates.npy', 'wb') as fl:
+            with open(f'/home/mila/c/chris.emezue/scratch/ate_estimates3/true_{baseline_to_use}_{seed}_ate_estimates.npy', 'wb') as fl:
                 np.save(fl,true_causal_estimates)
             rmse_value = calculate_rmse(causal_estimates,true_causal_estimates)
             
@@ -123,10 +143,12 @@ if __name__=="__main__":
             rmses_.append(rmse_value)
             seeds_.append(seed)
 
-            
-    df = pd.DataFrame({'baselines':baselines_,'rmse':rmses_,'seeds':seeds_})
-    df.to_csv(f'ate_estimates2/{baseline_to_use}_{SEED_TO_USE}_ate_estimates.csv',index=False)
-    print('ALL DONE')
+    if len(baselines_)!=0 and len(rmses_)!=0 and len(seeds_)!=0:        
+        df = pd.DataFrame({'baselines':baselines_,'rmse':rmses_,'seeds':seeds_})
+        df.to_csv(f'ate_estimates3/{baseline_to_use}_{SEED_TO_USE}_ate_estimates.csv',index=False)
+    else:
+        print("List was empty so nothing was done")
+    print(f'ALL DONE for seeds: {SEED_TO_USE}')
 
 
 
